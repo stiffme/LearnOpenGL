d0df7e77b22f5f4b319533c0f540242305e466ee
stiffme <PSCQJop234>
2019-05-16 19:58:02 +0800

    Multiple light

--- src/main/java/org/esipeng/opengl/guangzhao/MultipleLights.java
+++ src/main/java/org/esipeng/opengl/guangzhao/MultipleLights.java
@@ -13,12 +13,14 @@ import static org.lwjgl.system.MemoryUtil.NULL;
 public class MultipleLights extends OGLApplicationGL33 {
     private static final int MVP_BINDING_POINT = 1;
     private static final int DIR_LIGHT_BINDING_POINT = 2;
+    private static final int POINT_LIGHT_BINDING_POINT = 3;
+    private static final int SPOT_LIGHT_BINDING_POINT = 4;
 
-    private int m_width = 800, m_height = 800;
+    private int m_width = 1024, m_height = 1024;
     private long m_window ;
 
     private int m_vao, m_program, m_programLight;
-    private UBOManager m_modelViewProjectionUBO, m_dirLightUBO;
+    private UBOManager m_modelViewProjectionUBO, m_dirLightUBO, m_pointLightUBO, m_spotLightUBO;
     private Matrix4f m_model, m_projection;
     private Camera m_camera;
     private Vector3f m_rotateAxis = new Vector3f(1.0f,0.3f,0.5f).normalize();
@@ -221,25 +223,82 @@ public class MultipleLights extends OGLApplicationGL33 {
                 return false;
             glUniform1f(materialShiningness, 32.0f);
 
-            //create UBO for DirLight
-            int dirLightVBO = getManagedVBO();
-            m_dirLightUBO = new UBOManager();
-            if(!m_dirLightUBO.attachUniformBlock(m_program,"DirLight", dirLightVBO))
-                return false;
-            //bind the VBO to the DIR binding point
-            glBindBufferBase(GL_UNIFORM_BUFFER,DIR_LIGHT_BINDING_POINT, dirLightVBO);
-            //link the program to the DIR binding point
-            glUniformBlockBinding(m_program, m_dirLightUBO.getBlockIndex(),DIR_LIGHT_BINDING_POINT);
-            //dir direction will be calculated in update()
-            m_dirLightUBO.setValue("dirLight.ambient", 0.05f, 0.05f, 0.05f);
-            m_dirLightUBO.setValue("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
-            m_dirLightUBO.setValue("dirLight.specular", 0.5f, 0.5f, 0.5f);
-
-
         } catch (Exception e)   {
             e.printStackTrace();
             return false;
         }
+
+
+        //create UBO for DirLight
+        int dirLightVBO = getManagedVBO();
+        m_dirLightUBO = new UBOManager();
+        if(!m_dirLightUBO.attachUniformBlock(m_program,"DirLight", dirLightVBO))
+            return false;
+        //bind the VBO to the DIR binding point
+        glBindBufferBase(GL_UNIFORM_BUFFER,DIR_LIGHT_BINDING_POINT, dirLightVBO);
+        //link the program to the DIR binding point
+        glUniformBlockBinding(m_program, m_dirLightUBO.getBlockIndex(),DIR_LIGHT_BINDING_POINT);
+        //dir direction will be calculated in update()
+        m_dirLightUBO.setValue("dirLight.ambient", 0.05f, 0.05f, 0.05f);
+        m_dirLightUBO.setValue("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
+        m_dirLightUBO.setValue("dirLight.specular", 0.5f, 0.5f, 0.5f);
+
+        //create UBO for PointLight
+        int pointLightVBO = getManagedVBO();
+        m_pointLightUBO = new UBOManager();
+        if(!m_pointLightUBO.attachUniformBlock(m_program,"PointLight",pointLightVBO))
+            return false;
+        //bind the VBO to the binding point
+        glBindBufferBase(GL_UNIFORM_BUFFER,POINT_LIGHT_BINDING_POINT, pointLightVBO);
+        //link the program to the POINT binding point
+        glUniformBlockBinding(m_program,m_pointLightUBO.getBlockIndex(),POINT_LIGHT_BINDING_POINT);
+
+        // point light 1
+        m_pointLightUBO.setValue("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
+        m_pointLightUBO.setValue("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
+        m_pointLightUBO.setValue("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
+        m_pointLightUBO.setValue("pointLights[0].constant", 1.0f);
+        m_pointLightUBO.setValue("pointLights[0].linear", 0.09f);
+        m_pointLightUBO.setValue("pointLights[0].quadratic", 0.032f);
+        // point light 2
+        m_pointLightUBO.setValue("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
+        m_pointLightUBO.setValue("pointLights[1].diffuse", 0.8f, 0.8f, 0.8f);
+        m_pointLightUBO.setValue("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
+        m_pointLightUBO.setValue("pointLights[1].constant", 1.0f);
+        m_pointLightUBO.setValue("pointLights[1].linear", 0.09f);
+        m_pointLightUBO.setValue("pointLights[1].quadratic", 0.032f);
+        // point light 3
+        m_pointLightUBO.setValue("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
+        m_pointLightUBO.setValue("pointLights[2].diffuse", 0.8f, 0.8f, 0.8f);
+        m_pointLightUBO.setValue("pointLights[2].specular", 1.0f, 1.0f, 1.0f);
+        m_pointLightUBO.setValue("pointLights[2].constant", 1.0f);
+        m_pointLightUBO.setValue("pointLights[2].linear", 0.09f);
+        m_pointLightUBO.setValue("pointLights[2].quadratic", 0.032f);
+        // point light 4
+        m_pointLightUBO.setValue("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
+        m_pointLightUBO.setValue("pointLights[3].diffuse", 0.8f, 0.8f, 0.8f);
+        m_pointLightUBO.setValue("pointLights[3].specular", 1.0f, 1.0f, 1.0f);
+        m_pointLightUBO.setValue("pointLights[3].constant", 1.0f);
+        m_pointLightUBO.setValue("pointLights[3].linear", 0.09f);
+        m_pointLightUBO.setValue("pointLights[3].quadratic", 0.032f);
+
+        //create UBO for spotlight
+        m_spotLightUBO = new UBOManager();
+        int spotLightVBO =  getManagedVBO();
+        if(!m_spotLightUBO.attachUniformBlock(m_program, "SpotLight", spotLightVBO))
+            return false;
+        glBindBufferBase(GL_UNIFORM_BUFFER,SPOT_LIGHT_BINDING_POINT,spotLightVBO);
+        glUniformBlockBinding(m_program,m_spotLightUBO.getBlockIndex(), SPOT_LIGHT_BINDING_POINT);
+
+        //update spot light
+        m_spotLightUBO.setValue("spotLight.ambient", 0.0f, 0.0f, 0.0f);
+        m_spotLightUBO.setValue("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
+        m_spotLightUBO.setValue("spotLight.specular", 1.0f, 1.0f, 1.0f);
+        m_spotLightUBO.setValue("spotLight.constant", 1.0f);
+        m_spotLightUBO.setValue("spotLight.linear", 0.09f);
+        m_spotLightUBO.setValue("spotLight.quadratic", 0.032f);
+        m_spotLightUBO.setValue("spotLight.cutoff", (float)Math.cos(Math.toRadians(12.5f)));
+        m_spotLightUBO.setValue("spotLight.outerCutoff", (float)Math.cos(Math.toRadians(15.0f)));
 
         m_model = new Matrix4f();
         m_projection = new Matrix4f();
@@ -248,6 +307,7 @@ public class MultipleLights extends OGLApplicationGL33 {
         m_camera = new Camera(m_window);
         m_camera.enableMouseFpsView();
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        this.enableFps(true);
         return true;
     }
 
@@ -256,7 +316,33 @@ public class MultipleLights extends OGLApplicationGL33 {
         m_camera.processInput(elapsed);
         //calculate dir light direction
         m_dirLightDirection.set(-0.2f, -1.0f, -0.3f,0.f).mul(m_camera.generateViewMat());
-        m_dirLightUBO.setValue("dirLight.direction", -0.2f, -1.0f, -0.3f);
+        m_dirLightUBO.setValue("dirLight.direction",
+                m_dirLightDirection.x,
+                m_dirLightDirection.y,
+                m_dirLightDirection.z);
+
+        //calculate point light direction
+        for(int i = 0; i < m_pointLightPositions.length; ++i)   {
+            Vector3f pointPos = m_pointLightPositions[i];
+            m_dirLightDirection
+                    .set(pointPos,1.0f)
+                    .mul(m_camera.generateViewMat());
+
+            m_pointLightUBO.setValue(
+                    "pointLights["+ i + "].position",
+                    m_dirLightDirection.x,
+                    m_dirLightDirection.y,
+                    m_dirLightDirection.z
+            );
+        }
+
+        //calculate spot light front direction
+        float[] front = m_camera.getFront();
+        m_dirLightDirection.set(front[0],front[1],front[2],0.0f).mul(m_camera.generateViewMat());
+        m_spotLightUBO.setValue("spotLight.direction",
+                m_dirLightDirection.x,
+                m_dirLightDirection.y,
+                m_dirLightDirection.z);
     }
 
     @Override
@@ -278,6 +364,19 @@ public class MultipleLights extends OGLApplicationGL33 {
             m_modelViewProjectionUBO.setValue("model",m_model);
             glDrawArrays(GL_TRIANGLES,0,36);
         }
+
+        //draw lights
+        glUseProgram(m_programLight);
+        glBindVertexArray(m_vao);
+        for (Vector3f m_pointLightPosition : m_pointLightPositions) {
+            m_model.identity()
+                    .translate(m_pointLightPosition)
+                    .scale(0.2f);
+            m_modelViewProjectionUBO.setValue("model", m_model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+        glBindVertexArray(0);
+
         glFinish();
     }
 
--- src/main/resources/LightCasters/MultipleLights/fragment.glsl
+++ src/main/resources/LightCasters/MultipleLights/fragment.glsl
@@ -25,6 +25,59 @@ uniform DirLight    {
     DirLightStruct dirLight;
 };
 
+struct PointLightStruct {
+    vec3 position;
+    float constant;
+    float linear;
+    vec3 ambient;
+    float quadratic;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+#define NR_POINT_LIGHTS 4
+uniform PointLight{
+    PointLightStruct pointLights[NR_POINT_LIGHTS];
+};
+
+struct SpotLightStruct  {
+    //light position in view space is always at 0,0
+    vec3 direction;
+    float outerCutoff;
+    vec3 ambient;
+    float cutoff;
+    vec3 diffuse;
+    vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+uniform SpotLight {
+    SpotLightStruct spotLight;
+};
+
+vec3 CalcPointLight(PointLightStruct light, vec3 normal, vec3 viewDir)  {
+    vec3 lightDir = normalize(light.position - oFragPos);
+    float distance = length(light.position - oFragPos);
+    float attenuation = 1.0f / ( light.constant + distance * light.linear + distance * light.quadratic * light.quadratic);
+
+    //ambient
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, oTexCoord));
+
+    //diffuse
+    float diff = max(dot(lightDir, normal), 0.0f);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, oTexCoord));
+
+    //specular
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(reflectDir, viewDir), 0.0f), material.shininess);
+    vec3 specular = light.specular * spec *  vec3(texture(material.specular, oTexCoord));
+    return (ambient + diffuse + specular) * attenuation ;
+}
+
+
 vec3 CalcDirLight(DirLightStruct light, vec3 normal, vec3 viewDir)  {
     vec3 lightDir = normalize(-light.direction);
 
@@ -42,7 +95,38 @@ vec3 CalcDirLight(DirLightStruct light, vec3 normal, vec3 viewDir)  {
     return ambient + diffuse + specular;
 }
 
+vec3 CalcSpotLight(SpotLightStruct light, vec3 normal, vec3 viewDir)    {
+    vec3 lightDir = normalize(-oFragPos);
+    float distance = length(oFragPos);
+
+    float theta = dot(lightDir, normalize(-light.direction));
+    float intensity = clamp( (theta - light.outerCutoff) / (light.cutoff - light.outerCutoff), 0.0,1.0f);
+
+    float attenuation = 1.0 / (light.constant + distance * light.linear + distance * light.quadratic * light.quadratic);
+
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse,oTexCoord));
+
+    //diffuse
+    float diff = max(dot(lightDir,normal), 0.0);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, oTexCoord));
+
+    //specular
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = max(dot(reflectDir,normal), 0.0);
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, oTexCoord));
+
+    return (ambient + diffuse + specular) * intensity * attenuation;
+}
+
+
 void main() {
     vec3 viewDir = normalize(-oFragPos); //from frag to view, view is at 0,0,0 in view space
-    oColor =vec4(CalcDirLight(dirLight, oNormal, viewDir), 1.0f);
+    //directional light
+    vec3 result =CalcDirLight(dirLight, oNormal, viewDir);
+
+    for(int i = 0; i < NR_POINT_LIGHTS; ++i)
+        result += CalcPointLight(pointLights[i], oNormal, viewDir);
+
+    result += CalcSpotLight(spotLight, oNormal, viewDir);
+    oColor = vec4(result, 1.0f);
 }
